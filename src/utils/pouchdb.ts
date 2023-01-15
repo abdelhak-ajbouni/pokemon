0bf9317ad213c8cb8be0d49672343436027c0e3a
Abdelhak <abdelhak.ajbouni@gmail.com>
2023-01-15 19:27:48 +0100

    refactor pouchdb connection and closing

--- src/utils/pouchdb.ts
+++ src/utils/pouchdb.ts
@@ -3,12 +3,36 @@ import configs from "src/utils/configs";
 
 const db = new PouchDB(configs.db.host, configs.pouchDB);
 
-db.info().then((info) => {
-  console.log("PouchDB is connected!");
-  console.log(info);
-}).catch((err) => {
-  console.log("PouchDB is not connected!");
-  console.error(err);
+db.changes({
+  since: "now",
+  live: true,
+}).on("change", (change) => {
+  console.log("PouchDB change", change);
 });
 
+export const checkDB = async () => {
+  const info = await db.info().catch((err) => {
+    console.log("PouchDB is not connected!", err);
+  });
+
+  if (!info) {
+    await closeDB();
+  }
+
+  console.log("PouchDB is connected!");
+  console.log(info);
+};
+
+export const closeDB = async () => {
+  await db.close();
+  console.log("PouchDB is closed!");
+  process.exit(0);
+};
+
+export const destroyDB = async () => {
+  await db.destroy();
+  console.log("PouchDB is destroyed!");
+};
+
+checkDB();
 export default db;
