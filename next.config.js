5bd2332ce9d2f1b13eb9a6998d9363d48a745db5
Abdelhak <abdelhak.ajbouni@gmail.com>
2025-05-08 22:25:12 +0200

    Configure Next.js to use Babel instead of SWC

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false
+  swcMinify: false,
+  webpack: (config, { isServer }) => {
+    // Force Next.js to use Babel instead of SWC
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      use: 'babel-loader',
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig
